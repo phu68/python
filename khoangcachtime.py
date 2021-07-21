110555282fe0b243095d36311715d55481146f05
KS Phu <thanhphupham@gmail.com>
2021-07-21 20:17:42 +0700

    add file tinh khoang cach thoi gian

---
+++ khoangcachtime.py
@@ -0,0 +1,9 @@
+
+gio1 = int(input('Nhap gio 1: '))
+phut1 = int(input('Nhap phut 1:'))
+
+gio2 = int(input('Nhap gio 2: '))
+phut2 = int(input('Nhap phut 2:'))
+
+def truphut(phut1,phut2):
+	return phut2
---
+++ pandas.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+pd.DataFrame({'Yes': [50, 21], 'No': [131, 2]})
+
+pd.DataFrame({'Bob': ['I liked it.', 'It was awful.'], 'Sue': ['Pretty good.', 'Bland.']})
+
+
