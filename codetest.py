f3f334335b99a497d2592387b94cade97b9b2ae4
KS Phu <thanhphupham@gmail.com>
2021-07-22 22:33:17 +0700

    add pybook jupiter

--- codetest.py
+++ codetest.py
@@ -1,12 +1,21 @@
-def factorial(x):
-    """This is a recursive function
-    to find the factorial of an integer"""
+# # Program to filter out only the even items from a list
+# my_list = [1, 5, 4, 6, 8, 11, 3, 12]
 
-    if x == 1:
-        return 1
-    else:
-        return (x * factorial(x-1))
+# new_list = list(filter(lambda x: (x%2 == 0) , my_list))
+
+# print(new_list)
 
 
-num = 3
-print("The factorial of", num, "is", factorial(num))
+
+# i=0
+# while i < 5:
+#     danhsach = list(input("Nhap phan tu [",i,"]: "))
+#     i+=1
+#     pass
+
+# print(danhsach)
+
+
+aTuple = (123, 'xyz', 'zara', 'abc');
+aList = list(aTuple)
+print "List elements : ", aList
