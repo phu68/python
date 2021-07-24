439e5bfcb7ff867b52b780d4dc1583bb38c53d7a
KS Phu <thanhphupham@gmail.com>
2021-07-24 08:30:57 +0700

    add theme file pybook

--- codetest.py
+++ codetest.py
@@ -1,21 +1,4 @@
-# # Program to filter out only the even items from a list
-# my_list = [1, 5, 4, 6, 8, 11, 3, 12]
 
-# new_list = list(filter(lambda x: (x%2 == 0) , my_list))
+danhsach = ["1","b","3","d","h","j"]
 
-# print(new_list)
-
-
-
-# i=0
-# while i < 5:
-#     danhsach = list(input("Nhap phan tu [",i,"]: "))
-#     i+=1
-#     pass
-
-# print(danhsach)
-
-
-aTuple = (123, 'xyz', 'zara', 'abc');
-aList = list(aTuple)
-print "List elements : ", aList
+print(danhsach[:2])
