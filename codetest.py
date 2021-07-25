71e1746a22865a17449bf86557a0ae3bdbe9a82f
KS Phu <thanhphupham@gmail.com>
2021-07-25 15:00:47 +0700

    thêm bài tập duyệt chuỗi

--- codetest.py
+++ codetest.py
@@ -1,4 +1,12 @@
 
-danhsach = ["1","b","3","d","h","j"]
+# danhsach = ["1","b","3","d","h","j"]
 
-print(danhsach[:2])
+# print(danhsach[:2])
+
+
+# Iterating through a string
+count = 0
+for letter in 'Hello World':
+    if(letter == 'l'):
+        count += 1
+print(count,'letters found')
