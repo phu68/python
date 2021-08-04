b6794d6ea62ee53083b99502ca4135fe0c40941f
KS Phu <thanhphupham@gmail.com>
2021-08-04 10:49:21 +0700

    add folder programing_example include python code example

---
+++ programing_example/02_add_two_numbers.py
@@ -0,0 +1,17 @@
+# # Add two number
+
+# #store input number
+# num1 = input("Nhap so thu 1: ")
+# num2 = input("Nhap so thu 3: ")
+
+# # add two numbers
+# sum = float(num1) + float(num2)
+
+# # display the sum
+# print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+
+
+print('The sum is %.2f' %(float(input('Enter first number: ')) + float(input('Enter second number: '))))
+
+# %.2f làm tròn đến chữ số thập phân thứ 2
