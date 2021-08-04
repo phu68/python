2b4e0d55e7aa65e3c55e0b2bc91f1b148fb61d70
KS Phu <thanhphupham@gmail.com>
2021-08-04 18:27:44 +0700

    edit chuong trinh

--- programing_example/12_find_largest_among_three_years.py
+++ programing_example/12_find_largest_among_three_years.py
@@ -2,12 +2,19 @@ a = int(input("Input a: "))
 b = int(input("Input b: "))
 c = int(input("Input c: "))
 
-max = a
-if a < b:
-    if b > c:
-        max = b
-    elif a < c:
-        max = c
-elif a < c:
-    max = c
-print('Max is ',max)
+# max = a
+# if a < b:
+#     if b > c:
+#         max = b
+#     elif a < c:
+#         max = c
+# elif a < c:
+    # max = c
+# print('Max is ',max)
+
+if (a > b) and (a > c):
+    print("Max is ", a)
+elif (b > a) and (b > c):
+    print("Max is ", b)
+else:
+    print("Max is ",c)
