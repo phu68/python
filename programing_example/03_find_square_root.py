695115f64e8c854ebcb4cfc3dd25813cb2cf9b08
KS Phu <thanhphupham@gmail.com>
2021-08-04 16:56:13 +0700

    edit file 03

--- programing_example/03_find_square_root.py
+++ programing_example/03_find_square_root.py
@@ -8,7 +8,20 @@ import cmath
 # num = 1+2j
 
 # To take input from the user
-num = eval(input('Enter a number: '))
+num = eval(input('Enter a number: ')) # eval để tính số thực có dạng (a + bj)
 
 num_sqrt = cmath.sqrt(num)
 print('The square root of {0} is {1:0.3f}+{2:0.3f}j'.format(num ,num_sqrt.real,num_sqrt.imag))
+
+
+# num_sqrt.real = phần thực
+# num_sqrt.imag = phần ảo
+
+'''
+Trong chương trình này, chúng tôi sử dụng hàm sqrt () trong mô-đun cmath (toán phức hợp).
+'''
+
+'''
+Phương thức eval() có thể được sử dụng để chuyển đổi các số phức 
+làm đầu vào cho các đối tượng phức tạp trong Python. 
+'''
